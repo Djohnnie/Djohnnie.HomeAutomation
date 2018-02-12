84a1c5d7a7343a51182cf17c71150d0c7d7df6d2
Johnny Hooyberghs <johnny.hooyberghs@gmail.com>
2018-02-12 11:23:54 +0100

    Updated UI and tested Ajax request for power consumption

--- Djohnnie.HomeAutomation.Web/Controllers/HomeController.cs
+++ Djohnnie.HomeAutomation.Web/Controllers/HomeController.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Djohnnie.HomeAutomation.Web.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Djohnnie.HomeAutomation.Web.Controllers
 {
@@ -9,5 +15,31 @@ namespace Djohnnie.HomeAutomation.Web.Controllers
             AddPageHeader("Dashboard", "");
             return View();
         }
+
+        public async Task<IActionResult> GetLivePowerUsage()
+        {
+            LivePowerUsageViewModel vm = new LivePowerUsageViewModel()
+            {
+                CurrentPower = new Random().Next(100, 5000)
+            };
+
+            RestClient client = new RestClient("http://192.168.10.92/gateway/apipublic/reportInstantaneousValues");
+            var response = await client.ExecuteTaskAsync<Blablabla>(
+                new RestRequest(Method.GET) { RequestFormat = DataFormat.Json, OnBeforeDeserialization = resp => { resp.ContentType = "application/json"; } });
+            if (response.IsSuccessful && response.Data != null)
+            {
+                foreach (Match m in Regex.Matches(response.Data.Report, @"activePower=(\d*\.?\d+) W"))
+                {
+                    vm.CurrentPower = (int)Math.Round(Convert.ToDecimal(m.Groups[1].Value, CultureInfo.InvariantCulture));
+                }
+            }
+
+            return PartialView("_LivePowerUsage", vm);
+        }
+    }
+
+    public class Blablabla
+    {
+        public String Report { get; set; }
     }
 }
---
+++ Djohnnie.HomeAutomation.Web/ViewModels/LivePowerUsageViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Djohnnie.HomeAutomation.Web.ViewModels
+{
+    public class LivePowerUsageViewModel
+    {
+        public Int32 CurrentPower { get; set; }
+    }
+}
